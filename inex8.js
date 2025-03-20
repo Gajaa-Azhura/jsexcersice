f6cc7f332133df83017510ad7f1be94d2b2fa558
Gajaa-Azhura <shinr433@gmail.com>
2025-03-20 12:33:59 +0530

    more files

---
+++ inex8.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("mouseover", function(){
+this.style.backgroundColor="red";
+});
+document.getElementById("btn").addEventListener("mouseout", function(){
+this.style.backgroundColor="";
+});
